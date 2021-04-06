35d4a7df9d2727d4e4afe44d732b6440200658b0
sergiykudrych <81011171+sergiykudrych@users.noreply.github.com>
2021-04-06 16:22:31 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.header__burger').click(function(event) {
-        $('.header__burger,.header__menu').toggleClass('active');
+        $('.header__burger,.header__menu,.fixed').toggleClass('active');
         $('body').toggleClass('lock');
     });
 });
